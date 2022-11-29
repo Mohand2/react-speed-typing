8715a4c68ca955ab898317c492ae051221556e72
moh <alloun.mohand@gmail.com>
2022-11-29 11:27:39 +0100

    First commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div className="Game">
+      <h1>How fast do you type?</h1>
+
+      <textarea
+        name="typing-area"
+        id="typing-area"
+        className="textarea"
+        placeholder="Start typing..."
+      ></textarea>
+    </div>
+  );
+}
+
+export default Game;
